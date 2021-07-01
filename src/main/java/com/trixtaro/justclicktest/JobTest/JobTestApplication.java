b2c5edd91a96f721fc37c13490d3a6d121f28ccb
Trixtaro <wilmerdavid97@hotmail.com>
2021-06-30 22:12:00 -0500

    Initial commit

---
+++ src/main/java/com/trixtaro/justclicktest/JobTest/JobTestApplication.java
@@ -0,0 +1,13 @@
+package com.trixtaro.justclicktest.JobTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTestApplication.class, args);
+	}
+
+}
