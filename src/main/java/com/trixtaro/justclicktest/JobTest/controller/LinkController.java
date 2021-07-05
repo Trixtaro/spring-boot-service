35b5393c5dfbae04c195a94091c1139cc6aa8453
Trixtaro <wilmerdavid97@hotmail.com>
2021-07-04 23:00:46 -0500

    Ahora se puede guardar las peticiones al archivo click.json

--- src/main/java/com/trixtaro/justclicktest/JobTest/controller/LinkController.java
+++ src/main/java/com/trixtaro/justclicktest/JobTest/controller/LinkController.java
@@ -2,19 +2,24 @@ package com.trixtaro.justclicktest.JobTest.controller;
 
 import com.trixtaro.justclicktest.JobTest.entity.Link;
 import com.trixtaro.justclicktest.JobTest.repository.LinkRepository;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/link")
@@ -28,7 +33,7 @@ public class LinkController {
     }
 
     @GetMapping("/{string}")
-    public ResponseEntity get(@PathVariable String string, HttpServletRequest request, HttpServletResponse response) throws IOException{
+    public ResponseEntity get(@PathVariable String string, @RequestHeader Map<String, String> headers, HttpServletResponse response) throws IOException{
 
         List<Link> links = linkRepository.findByValue(string);
 
@@ -39,12 +44,50 @@ public class LinkController {
             currentLink.setRemainingCalls(currentLink.getRemainingCalls() - 1);
             linkRepository.save(currentLink);
 
+            JSONObject json = new JSONObject();
+
+            headers.forEach((key, value) -> {
+                json.put(key, value);
+            });
+
+            // Ading request to json file
+            JSONArray jsonArray = readJsonFile("click.json");
+            jsonArray.add(json);
+            saveToJsonFile("click.json", jsonArray);
+
             response.sendRedirect(links.get(0).getUrl());
             return new ResponseEntity(HttpStatus.PERMANENT_REDIRECT);
         } else {
-            return new ResponseEntity("404 NOT FOUND", HttpStatus.NOT_FOUND);
+            return new ResponseEntity("4047 NOT FOUND", HttpStatus.NOT_FOUND);
         }
 
+    }
+
+    public void saveToJsonFile(String filename, JSONArray json){
+        try (FileWriter file = new FileWriter(filename)) {
+            file.write(json.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JSONArray readJsonFile(String filename){
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader(filename)){
+
+            Object obj = jsonParser.parse(reader);
+            JSONArray jsonArray = (JSONArray) obj;
+
+            return jsonArray;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new JSONArray();
     }
 
 }
